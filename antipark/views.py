5b41f706041fa907f8dcd2b77a8150200dfa10bf
vlfedotov <fedotov.vu@gmail.com>
2016-09-20 22:31:41 +0300

    вывод продуктов на странице /goods-item/<category_id>

--- antipark/views.py
+++ antipark/views.py
@@ -20,9 +20,11 @@ def about():
     return render_template('goods.html', categories=all_categories)
 
 
-@app.route('/goods-item/')
-def goods_item():
-    return render_template('goods-item.html', goods=all_goods)
+@app.route('/goods-item/<category_id>')
+def goods_item(category_id):
+    category = Category.query.get(category_id)
+    goods = Product.query.filter_by(category=category_id).all()
+    return render_template('goods-item.html', category=category, goods=goods)
 
 
 @app.route('/goods-category/')
