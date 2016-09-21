fc61551f347f2897d2f6b08e3276de91e024f687
vlfedotov <fedotov.vu@gmail.com>
2016-09-21 11:18:40 +0300

    вьюшки под сохранение бд в файл обновление бд из файла

--- antipark/views.py
+++ antipark/views.py
@@ -32,6 +32,38 @@ def goods_category():
     return render_template('goods-category.html', categories=all_categories)
 
 
-@app.route('/file')
+@app.route('/file/', methods = ['GET', 'POST'])
 def file():
+    if request.method == 'POST':
+        import pandas 
+
+        # вместо cur_base.xlsx нужна ссылка на загружаемый файл
+        df_new = pd.read_excel('cur_base.xlsx', index_col='id')
+
+        for prod in df_new.itertuples():
+            upd_product = Product.query.get(prod.Index)
+            for field in prod._fields[1:]:
+                setattr(upd_product, field, getattr(prod, field))
+            db.session.commit()
+
+        return 'Saved'
     return render_template('file.html')
+
+
+@app.route('/get_db/', methods=['GET', 'POST'])
+def get_db():
+    # if request.method == 'POST':
+    import pandas as pd
+    from collections import defaultdict
+
+    price_cols = ['id', 'title', 'price', 'stage_price']
+
+    prods = defaultdict(list)
+    for col in price_cols:
+        for product in all_goods:
+            prods[col].append(getattr(product, col))    
+
+    df = pd.DataFrame(prods)
+    df.set_index('id', inplace=True)
+    df.to_excel('cur_base.xlsx')
+    return 'Done'
