0fe8800371fdccbd73e63b5e9966d327259fdfd6
vlfedotov <fedotov.vu@gmail.com>
2016-09-21 23:13:07 +0300

    правки в вывод категорий и товаров

--- antipark/views.py
+++ antipark/views.py
@@ -20,19 +20,24 @@ def about():
     return render_template('goods.html', categories=all_categories)
 
 
-@app.route('/goods-item/<category_id>')
-def goods_item(category_id):
+@app.route('/goods-category/<category_id>/')
+def goods_category(category_id):
     category = Category.query.get(category_id)
     goods = Product.query.filter_by(category=category_id).all()
-    return render_template('goods-item.html', category=category, goods=goods)
+    return render_template('goods-category.html', category=category, goods=goods)
 
 
-@app.route('/goods-category/')
-def goods_category():
-    return render_template('goods-category.html', categories=all_categories)
+@app.route('/goods-item/<goods_item_id>/', methods=['GET', 'POST'])
+def goods_item(goods_item_id):
+    if request.method == 'GET':
+        goods_item = Product.query.get(str(goods_item_id))
+        category_id = goods_item.category
+        category = Category.query.get(category_id)
+        return render_template('goods-item.html', category=category,
+                                                    goods_item=goods_item)
 
 
-@app.route('/file/', methods = ['GET', 'POST'])
+@app.route('/file/', methods=['GET', 'POST'])
 def file():
     if request.method == 'POST':
         import pandas 
